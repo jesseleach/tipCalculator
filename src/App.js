423c691c2fce03a944193e2dea0c8887517c11ea
jesseleach <jesserobertleach@gmail.com>
2023-08-05 14:25:24 -0400

    version 2.0

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [tip, setTip] = useState();
+  const [tip, setTip] = useState('');
+  const [updated, setUpdated] = useState(tip);
+
   const handleNameChange = (evt) => {
     const newValue = evt.target.value;
     setTip(newValue * .20);
   }
 
+  const handleClick = () => {
+setUpdated(tip);
+  };
+
   return (
     <div>
       <h1>Tip Calculator</h1>
-      <p>The tip is {tip}</p>
+      <p>The tip is {updated}</p>
       <input onChange={handleNameChange}/>
+      <button onClick={handleClick}>update</button>
     </div>
   );
 }
