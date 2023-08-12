a5ddfb66cf1f75361353e047b6b31891bb8eb583
jesseleach <jesserobertleach@gmail.com>
2023-08-12 14:13:57 -0400

    version 4.0

--- src/App.js
+++ src/App.js
@@ -2,24 +2,33 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [tip, setTip] = useState('');
-  const [updated, setUpdated] = useState(tip);
+  const [input, setInput] = useState("")
+  const [tip, setTip] = useState();
 
-  const handleNameChange = (evt) => {
+  const handleInputChange = (evt) => {
     const newValue = evt.target.value;
-    setTip(newValue * .20);
-  }
+    setInput(newValue);
+  };
 
-  const handleClick = () => {
-setUpdated(tip);
+  const getTipClickHandler = (percentage) => () => {
+    setTip(input * percentage);
+  }
+    const handleClickReset = () => {
+    setTip(null);
+    setInput("");
+
   };
 
   return (
     <div>
       <h1>Tip Calculator</h1>
-      <p>The tip is {updated}</p>
-      <input onChange={handleNameChange}/>
-      <button onClick={handleClick}>update</button>
+      <p>The tip is {tip}</p>
+      <input value={input} onChange={handleInputChange} />
+      <button disabled={!input} onClick={getTipClickHandler(0.25)}>25%</button>
+      <button disabled={!input} onClick={getTipClickHandler(0.20)}>20%</button>
+      <button disabled={!input} onClick={getTipClickHandler(0.15)}>15%</button>
+      <button disabled={!input} onClick={getTipClickHandler(0.10)}>10%</button>
+      <button disabled={!input} onClick={handleClickReset}>Reset</button>
     </div>
   );
 }
